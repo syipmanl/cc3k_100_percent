608de69a7eaddda4cfcf3b236d4046833b98bc5a
Alex Lai <qq731008253@gmail.com>
2017-07-15 23:18:03 -0400

    Change it if it makes it simpler

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,10 @@
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 
+// prints the usage of executing this file
 void usage() {
 	cerr << "........." << endl;
 }
@@ -11,13 +13,16 @@ void usage() {
 int main(int argc, char *argv[]) {
 	typedef int OPTION;
 	// for sure to change it
-	enum {Default=0, LoadfromSavedfile, LoadMap, Bonus, Test1, Test2/*,Test3... etc.*/};
+	//
+	enum {Default=0, LoadfromSavedfile, LoadMap, Bonus, Test1, Test2/*,Test3... etc., extra feature*/};
 
 	OPTION option;
 	
 	if (argc==1) {
 		option=Default;
 	} else if (argc==2) {
+		char *argument=argv[1];
+		// 	
 	} else if (argc==3) {
 	} else {
 		cerr << "Incorrect number of arguments"<<endl;
