f211fa70133bf8c2987dad06a0e25afcdedc5021
Alex Lai <qq731008253@gmail.com>
2017-07-15 22:42:09 -0400

    Change name of directory

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include <string>
+
+using std::string;
+
+
+int main(int argc, char *argv[]) {
+	typedef int OPTION;
+	// for sure to change it
+	enum {Default=0, LoadfromSavedfile, LoadMap, Bonus, Test1, Test2/*,Test3... etc.*/};
+
+	OPTION option;
+	
+	if (argc
+
+	switch 
