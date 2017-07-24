f49d0d281e3366cd8eff8da83a6d867c7efaa95a
syipmanl <stanley3558@Hotmail.com>
2017-07-24 06:02:27 -0400

    still need to edit enemy.h

--- src/enemy.h
+++ src/enemy.h
@@ -8,24 +8,14 @@ class DragonHoard;
 
 class Enemy: public Character {
 private:
-	bool act_flag;
+
 public: 
-	
 	Enemy(int Atk, int Def, int HP, std::string race, char symbol, std::string type);
-	
 	virtual ~Enemy() = 0;
-
-	bool isActed();
-
-	void act();
-
-	void actClear();
-
 	virtual void dropGold();
 };
 
 
-// Human: 140 HP, 20 Atk, 20 Def, 2 golds * 2
 class Human : public Enemy{
 	public:
 		Human();
@@ -33,14 +23,12 @@ class Human : public Enemy{
 		void dropGold();
 };
 
-// Dwarf: 100 HP, 20 Atk, 30 Def, Vampire -5 HP upon attacking
 class Dwarf : public Enemy{
 	public:
 		Dwarf();
 		~Dwarf();
 };
 
-// Elf: 140 HP, 30 Atk, 10 Def, attack twice except Drow
 class Elf : public Enemy{
 	public:
 		Elf();
@@ -48,14 +36,12 @@ class Elf : public Enemy{
 
 };
 
-// Orc: 180 HP, 30 Atk, 25 Def, 50% more dmg to goblin
 class Orc : public Enemy{
 	public:
 		Orc();
 		~Orc();
 };
 
-// Merchant: 30 HP, 70 Ark, 5 Def, defaultly not hostile 
 class Merchant : public Enemy{
 	public:
 		static bool hostile;
@@ -64,14 +50,12 @@ class Merchant : public Enemy{
 		void dropGold();
 };
 
-// Halfing: 100 HP, 15 Ark, 20 Def, player have 50% chance to miss
 class Halfling : public Enemy{
 	public:
 		Halfling();
 		~Halfling();
 };
 
-// Dragon: 150 HP, 20 Atk, 20 Def, stationary, spawn when dragon hoard is spawned
 class Dragon : public Enemy{
 	private:
 		DragonHoard* hoard;
@@ -81,7 +65,6 @@ class Dragon : public Enemy{
 		~Dragon();
 		DragonHoard* getHoard();
 		void setHoard(DragonHoard* hd);
-		void dropGold();
 };
 
-#endif //_ENEMY_H
+#endif 
