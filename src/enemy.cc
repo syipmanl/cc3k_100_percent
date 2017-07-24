f532cd8d5a84dfbfffd8d38f3231fe3147d9ee50
syipmanl <stanley3558@Hotmail.com>
2017-07-24 05:59:52 -0400

    need a little bit more update on enemy.cc/h and gold.cc/h

--- src/enemy.cc
+++ src/enemy.cc
@@ -6,96 +6,85 @@ using namespace std;
 
 
 Enemy::Enemy(int ATK, int DEF, int HP, string race, char symbol, string type, bool hostile)
-			: Character(Atk, Def, HP, race, symbol, type), hostile(hostile)
-	{}
-
-Enemy::~Enemy() {}
+			: Character(ATK, DEF, HP, 0, 0, false, race, symbol, type), hostile(hostile) {}
 
 
+// default drop gold
 void Enemy::dropGold(){
-	int chance = rand() % 2;
-	Treasure* t = NULL;
-	if(chance == 0){
-		t = new SmallHoard();
+	int drop_rate = rand() % 2;
+	Gold* pile = NULL;
+	if(drop_rate != 0){
+		t = new Normal();
 	}
 	else{
-		t = new NormalHoard();
+		t = new SmallHoard();
 	}
-	t->setPosition(position);
-	position->setObject(t);
+	pile->setPosition(position);
+	position->setObject(pile);
 }
 
-Human::Human() : Enemy(20, 20, 140, "H", 'Enemy', "Human", true){}
+Human::Human() : Enemy(20, 20, 140, "Human", 'H', "Enemy", true){}
 
 Human::~Human(){}
 
 void Human::dropGold(){
-	Piles *p1 = new NormalHoard();
-	Piles *p2 = new NormalHoard();
-
+	Gold *p1 = new NormalHoard();
+	Gold *p2 = new NormalHoard();
 	p1->setPosition(position);
 	position->setObject(p1);
 
-	for(int i = 0; i < 8; i++){
+	for(int i = 0; i < 8; i++){ // see which one is free to drop the gold
 		Tile* current = position->getNeighbour(i);
 		if(current && current->getCh() == '.' && !current->getObject()){
 			p2->setPosition(current);
 			current->setObject(p2);
-			return;
 		}
 	}
-	delete p2;
 }
 
 
-Dwarf::Dwarf() : Enemy(20, 30, 100, "W", 'Enemy', "Dwarf", true){}
+Dwarf::Dwarf() : Enemy(20, 30, 100, "Dwarf", 'W', 'Enemy', true){}
 
 Dwarf::~Dwarf(){}
 
 
-Elf::Elf() : Enemy(30, 10, 140, 'E', "enemy", 'Elf', true){}
+Elf::Elf() : Enemy(30, 10, 140, "Elf", 'E', "Enemy", true){}
 
 Elf::~Elf(){}
 
 
-Orc::Orc() : Enemy(30, 25, 180, 'O', "enemy", "Orc", true){}
+Orc::Orc() : Enemy(30, 25, 180, "Orc", 'O', "Enemy", true){}
 
 Orc::~Orc(){}
 
-
-
-
-Merchant::Merchant() : Enemy(70, 5, 30, 'M', "enemy","Merchant",false){}
+Merchant::Merchant() : Enemy(70, 5, 30, "Merchant", 'M', "Enemy",false){}
 
 Merchant::~Merchant(){}
 
 void Merchant::dropGold(){
-	MerchantHoard* p = new MerchantHoard();
-	p->setPosition(position);
-	position->setObject(p);
-	return;
+	MerchantHoard* pile = new MerchantHoard();
+	pile->setPosition(position);
+	position->setObject(pile);
 }
 
-Halfling::Halfling() : Enemy(15, 20, 100, 'L', "enemy", "Halfling", true){}
+Halfling::Halfling() : Enemy(15, 20, 100, "Halfling", 'L', "Enemy", true){}
 
 Halfling::~Halfling(){}
 
+// constructor with no hoard
+Dragon::Dragon() : Enemy(20, 20, 150, 'Dragon', 'D', "Enemy", true), hoard(NULL){}
 
-Dragon::Dragon() : Enemy(20, 20, 150, 'D', "enemy", "Dragon", true), hoard(NULL){}
-
-Dragon::Dragon(DragonHoard* hoard) : Enemy(20, 20, 150, "Dragon", 'D', "enemy"), hoard(hoard) {}
+// constructor with hoard
+Dragon::Dragon(DragonHoard* hoard) : Enemy(20, 20, 150, "Dragon", 'D', "enemy", true), hoard(hoard) {}
 
 Dragon::~Dragon(){}
-
-void Dragon::dropGold(){
-	return;
-}
 
 DragonHoard* Dragon::getHoard(){
 	return hoard;
 }
 
-void Dragon::setHoard(DragonHoard* hd){
-	hoard = hd;
+void Dragon::setHoard(DragonHoard* newhoard){
+	hoard = newhoard;
 }
 
+Enemy::~Enemy() {}
