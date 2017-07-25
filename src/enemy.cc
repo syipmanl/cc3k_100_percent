83cc1b237dc4c21197bb78fbd6a4434b4d5dedbf
syipmanl <stanley3558@Hotmail.com>
2017-07-24 20:49:08 -0400

    update

--- src/enemy.cc
+++ src/enemy.cc
@@ -5,16 +5,16 @@
 using namespace std;
 
 
-Enemy::Enemy(int ATK, int DEF, int HP, string race, char symbol, string type, bool hostile)
-			: Character(ATK, DEF, HP, 0, 0, false, race, symbol, type), hostile(hostile) {}
+Enemy::Enemy(int ATK, int DEF, int HP, char symbol, string race, string type, bool hostile)
+			: Character(ATK, DEF, HP, 0, 0, false, symbol, race, type), hostile(hostile) {}
 
 
 // default drop gold
-void Enemy::dropGold(){
+void Enemy::deadGold(){
 	int drop_rate = rand() % 2;
 	Gold* pile = NULL;
 	if(drop_rate != 0){
-		t = new Normal();
+		t = new NormalHoard();
 	}
 	else{
 		t = new SmallHoard();
@@ -23,11 +23,11 @@ void Enemy::dropGold(){
 	position->setObject(pile);
 }
 
-Human::Human() : Enemy(20, 20, 140, "Human", 'H', "Enemy", true){}
+Human::Human() : Enemy(20, 20, 140, 'H', "Human", "Enemy", true){}
 
 Human::~Human(){}
 
-void Human::dropGold(){
+void Human::deadGold(){
 	Gold *p1 = new NormalHoard();
 	Gold *p2 = new NormalHoard();
 	p1->setPosition(position);
@@ -43,48 +43,48 @@ void Human::dropGold(){
 }
 
 
-Dwarf::Dwarf() : Enemy(20, 30, 100, "Dwarf", 'W', 'Enemy', true){}
+Dwarf::Dwarf() : Enemy(20, 30, 100, 'W', "Dwarf", 'Enemy', true){}
 
 Dwarf::~Dwarf(){}
 
 
-Elf::Elf() : Enemy(30, 10, 140, "Elf", 'E', "Enemy", true){}
+Elf::Elf() : Enemy(30, 10, 140, 'E', "Elf", "Enemy", true){}
 
 Elf::~Elf(){}
 
 
-Orc::Orc() : Enemy(30, 25, 180, "Orc", 'O', "Enemy", true){}
+Orc::Orc() : Enemy(30, 25, 180, 'O', "Orc" "Enemy", true){}
 
 Orc::~Orc(){}
 
-Merchant::Merchant() : Enemy(70, 5, 30, "Merchant", 'M', "Enemy",false){}
+Merchant::Merchant() : Enemy(70, 5, 30, 'M', "Merchant", "Enemy",false){}
 
 Merchant::~Merchant(){}
 
-void Merchant::dropGold(){
+void Merchant::deadGold(){
 	MerchantHoard* pile = new MerchantHoard();
 	pile->setPosition(position);
 	position->setObject(pile);
 }
 
-Halfling::Halfling() : Enemy(15, 20, 100, "Halfling", 'L', "Enemy", true){}
+Halfling::Halfling() : Enemy(15, 20, 100, 'L', "Halfling", "Enemy", true){}
 
 Halfling::~Halfling(){}
 
 // constructor with no hoard
-Dragon::Dragon() : Enemy(20, 20, 150, 'Dragon', 'D', "Enemy", true), hoard(NULL){}
+Dragon::Dragon() : Enemy(20, 20, 150, 'D', "Dragon", "Enemy", true), drag_hoard(NULL){}
 
 // constructor with hoard
-Dragon::Dragon(DragonHoard* hoard) : Enemy(20, 20, 150, "Dragon", 'D', "enemy", true), hoard(hoard) {}
+Dragon::Dragon(DragonHoard* hoard) : Enemy(20, 20, 150, "D", "Dragon", "enemy", true), drag_hoard(hoard) {}
 
 Dragon::~Dragon(){}
 
 DragonHoard* Dragon::getHoard(){
-	return hoard;
+	return drag_hoard;
 }
 
 void Dragon::setHoard(DragonHoard* newhoard){
-	hoard = newhoard;
+	drag_hoard = newhoard;
 }
 
 Enemy::~Enemy() {}
--- src/shade.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SHADE_H__
-#define __SHADE_H__
-#include "player.h"
-
-class Shade: public Player {
-public:
-	Shade();
-	~Shade();
-}
