9e1f88f328bb4c679e92628a748964a9451c4925
Alex Lai <qq731008253@gmail.com>
2017-07-24 22:12:34 -0400

    Change Tile.cc Tile.c

--- src/tile.h
+++ src/tile.h
@@ -1,7 +1,6 @@
 #ifndef __TILE_H__
 #define __TILE_H__
 
-#include "info.h"
 #include <vector>
 #include "textdisplay.h"
 #include "object.h"
@@ -19,7 +18,7 @@ public:
     bool inChamber; //
     char getSymbol();
     Object* getObject();
-    void set_Tile (const int new_row, const int new_col, const char new_symbol); //
+    void set_tile (const int new_row, const int new_col, const char new_symbol); //
     void attach(Tile *new_neighbours); 
     void attach_td(TextDisplay *td);
     void change_td(int row, int col, char ch);
